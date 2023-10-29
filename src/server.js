6b17101372fc249f6fc7ffec0fdf50b7d52b57d3
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-10-29 13:40:55 -0500

    fixed picture in db 1

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 const main = async() => {
     try{
-        await sequelize.sync()
+        await sequelize.sync({force: true})
         app.listen(PORT);
         await new Promise((resolve, reject) => {
             const migrate = exec(
